03bcc0efcfa86dba15b94ae8c301f001c8cb2560
NthPortal <nthportal@gmail.com>
2016-05-26 01:57:29 -0400

    Write solution to problem 2

---
+++ src/main/scala/io/github/nthportal/euler/p2/Problem2.scala
@@ -0,0 +1,11 @@
+package io.github.nthportal.euler.p2
+
+object Problem2 {
+  def apply(): Int = {
+    Stream.iterate[(Int, Int)]((1, 1)) { t => (t._2, t._1 + t._2) }
+      .takeWhile(_._2 < 4000000)
+      .map(_._2)
+      .filter(_ % 2 == 0)
+      .sum
+  }
+}
