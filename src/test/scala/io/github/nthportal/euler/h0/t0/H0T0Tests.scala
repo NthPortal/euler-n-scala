85fb125d766c11d520cf5009848a186457a7418c
NthPortal <nthportal@gmail.com>
2016-05-26 16:58:20 -0400

    Write solution to problem 4; add tests

---
+++ src/test/scala/io/github/nthportal/euler/h0/t0/H0T0Tests.scala
@@ -0,0 +1,18 @@
+package io.github.nthportal.euler.h0.t0
+
+import org.junit.Assert._
+import org.junit.Test
+
+class H0T0Tests {
+  @Test
+  def Problem1Test(): Unit = assertEquals(233168, Problem1())
+
+  @Test
+  def Problem2Test(): Unit = assertEquals(4613732, Problem2())
+
+  @Test
+  def Problem3Test(): Unit = assertEquals(6857, Problem3())
+
+  @Test
+  def Problem4Test(): Unit = assertEquals(906609, Problem4())
+}
