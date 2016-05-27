6da002d0da6327af39f503d03ab0c873dbc14978
NthPortal <nthportal@gmail.com>
2016-05-27 05:23:37 -0400

    Tweak prime stream method
    Change prime stream method to only check numbers
    against previously found primes.

--- src/main/scala/io/github/nthportal/euler/h0/t0/Problem7.scala
+++ src/main/scala/io/github/nthportal/euler/h0/t0/Problem7.scala
@@ -5,10 +5,8 @@ object Problem7 extends ProjectEulerProblem {
   private val goal = 10001
 
   override def apply(): Long = {
-    primesIn(2 #:: Stream.iterate(3L){_ + 2})
+    Util.primes
       .drop(goal - 1)
       .head
   }
-
-  private def primesIn(s: Stream[Long]): Stream[Long] = s.filter(Util.firstFactor(_).isEmpty)
 }
--- src/main/scala/io/github/nthportal/euler/h0/t0/Util.scala
+++ src/main/scala/io/github/nthportal/euler/h0/t0/Util.scala
@@ -8,16 +8,29 @@ object Util {
       case i if i < 0 => primeFactors(-i)
       case 0 | 1 => Stream.empty
       case n =>
-        firstFactor(n) match {
+        firstFactor(n, basicPrimeSearchStream) match {
           case Some(factor) => factor #:: primeFactors(n / factor)
           case None => Stream(n)
         }
     }
   }
 
-  def firstFactor(num: Long): Option[Long] = {
-    basicPrimeSearchStream
+  private def firstFactor(num: Long, searchStream: Stream[Long]): Option[Long] = {
+    searchStream
       .takeWhile(_ <= math.sqrt(num))
       .find(num % _ == 0)
+  }
+
+  def primes: Stream[Long] = primesIn(basicPrimeSearchStream, Stream.empty)
+
+  private def primesIn(toSearch: Stream[Long], prevPrimes: Stream[Long]): Stream[Long] = {
+    toSearch.headOption match {
+      case Some(head) =>
+        firstFactor(head, prevPrimes) match {
+          case Some(_) => primesIn(toSearch.tail, prevPrimes)
+          case None => head #:: primesIn(toSearch.tail, prevPrimes #::: Stream(head))
+        }
+      case None => Stream.empty
+    }
   }
 }
