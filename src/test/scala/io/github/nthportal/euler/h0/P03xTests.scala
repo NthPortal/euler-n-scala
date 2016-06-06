438a1b628ee3738f15eb0fdb7f96fe0d306689a1
NthPortal <nthportal@gmail.com>
2016-06-06 03:53:33 -0400

    Write solution to problem 30

---
+++ src/test/scala/io/github/nthportal/euler/h0/P03xTests.scala
@@ -0,0 +1,10 @@
+package io.github.nthportal.euler.h0
+
+import io.github.nthportal.euler.h0.t3._
+import org.junit.Assert._
+import org.junit.Test
+
+class P03xTests {
+  @Test
+  def problem30Test(): Unit = assertEquals(443839, Problem30())
+}
