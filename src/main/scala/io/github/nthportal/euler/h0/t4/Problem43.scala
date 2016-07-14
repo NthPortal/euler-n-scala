2b9de9d3030c66aee78a731fd67afaf5a02c0ecc
NthPortal <nthportal@gmail.com>
2016-07-14 00:06:26 -0400

    Optimize solution to problem 43

--- src/main/scala/io/github/nthportal/euler/h0/t4/Problem43.scala
+++ src/main/scala/io/github/nthportal/euler/h0/t4/Problem43.scala
@@ -9,11 +9,22 @@ object Problem43 extends ProjectEulerProblem {
   override def apply(): Long = {
     val primes = streams.primes
 
-    maths.permutationsOf(0 to 9)
+    optimizedPermutations
       .filterNot(_.head == 0)
       .filter(isSubstringPrimeDivisible(_, primes))
       .map(NumericFormat.fromDigits)
       .sum
+  }
+
+  /**
+    * The 6th digit must be `5` because `d4 d5 d6` must be divisible by 5.
+    * It cannot be a `0` because `d6 d7 d8` must be divisible by `11`, `d7`
+    * and `d8` must be different, and if `d6` was `0`, then `d7 d8` would be
+    * a 2-digit number, and could not be divisible by `11`.
+    */
+  private def optimizedPermutations: Stream[Seq[Int]] = {
+    maths.permutationsOf((0 to 4) ++: (6 to 9))
+      .map(seq => seq.slice(0, 5) ++: 5 +: seq.slice(5, 10))
   }
 
   private def isSubstringPrimeDivisible(digits: Seq[Int], primes: Primes): Boolean = {
