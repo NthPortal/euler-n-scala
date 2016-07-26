7a2bccf8a96ad58cad1699ef2b711992fb6d848b
NthPortal <nthportal@gmail.com>
2016-07-25 21:30:49 -0400

    Tweak math functions
    Optimize streams.Primes.
    
    Make implicit classes in main package object extend
    AnyVal.
    
    Add apply() method to ReusableStream which calls the
    'get' method.

--- src/main/scala/io/github/nthportal/euler/package.scala
+++ src/main/scala/io/github/nthportal/euler/package.scala
@@ -32,24 +32,27 @@ package object euler {
     def **(b: Double) = math.pow(a, b)
   }
 
+  implicit def int2Long(int: Int): Long = int.toLong
+  implicit def long2Int(long: Long): Int = long.toInt
+
   /* elem in seq */
 
-  implicit class CharSeqInString(seq: CharSequence) {
+  implicit class CharSeqInString(private val seq: CharSequence) extends AnyVal {
     def in(other: String): Boolean = other contains seq
     def notIn(other: String): Boolean = !in(other)
   }
 
-  implicit class ObjInSeqLike[A, A1 >: A, Repr](obj: A1) {
+  implicit class ObjInSeqLike[A, A1 >: A, Repr,T](private val obj: A1) extends AnyVal {
     def in(seqLike: SeqLike[A, Repr]): Boolean = seqLike contains obj
     def notIn(seqLike: SeqLike[A, Repr]): Boolean = !in(seqLike)
   }
 
-  implicit class ObjInGenSetLike[A, Repr](obj: A) {
+  implicit class ObjInGenSetLike[A, Repr](private val obj: A) extends AnyVal {
     def in(genSetLike: GenSetLike[A, Repr]): Boolean = genSetLike contains obj
     def notIn(genSetLike: GenSetLike[A, Repr]): Boolean = !in(genSetLike)
   }
 
-  implicit class KeyInGenMapLike[A, B, Repr](key: A) {
+  implicit class KeyInGenMapLike[A, B, Repr](private val key: A) extends AnyVal {
     def in(genMapLike: GenMapLike[A, B, Repr]): Boolean = genMapLike contains key
     def notIn(genMapLike: GenMapLike[A, B, Repr]): Boolean = !in(genMapLike)
   }
--- src/main/scala/io/github/nthportal/euler/util/maths/streams/Primes.scala
+++ src/main/scala/io/github/nthportal/euler/util/maths/streams/Primes.scala
@@ -4,5 +4,6 @@ import io.github.nthportal.euler.util.maths
 
 class Primes private[streams]() extends ReusableStream[Long] {
   import maths.Primes._
-  val get: Stream[Long] = 2 #:: oddNumbers.filter(firstFactor(_, get).isEmpty)
+  private val stream: Stream[Long] = 3 #:: oddNumbers.drop(1).filter(firstFactor(_, stream).isEmpty)
+  val get: Stream[Long] = 2 #:: stream
 }
