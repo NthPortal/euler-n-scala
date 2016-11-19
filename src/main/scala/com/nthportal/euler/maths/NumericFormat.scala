313c9f0ef71723a4ff65e236ae3aeb4eaec81f58
NthPortal <nthportal@gmail.com>
2016-11-19 06:16:38 -0500

    Improve digitsOf methods
    Add binaryDigitsOf method.

--- src/main/scala/com/nthportal/euler/maths/NumericFormat.scala
+++ src/main/scala/com/nthportal/euler/maths/NumericFormat.scala
@@ -4,6 +4,7 @@ package maths
 import scala.annotation.tailrec
 
 object NumericFormat {
+  private val big0 = BigInt(0)
   private val big10 = BigInt(10)
 
   def twoDigitStringAsNum(s: String): Int = {
@@ -24,7 +25,7 @@ object NumericFormat {
   }
 
   def isBinaryPalindrome(num: Long): Boolean = {
-    val str = num.toBinaryString
+    val str = binaryDigitsOf(num)
     str == str.reverse
   }
 
@@ -37,14 +38,20 @@ object NumericFormat {
 
   def digitsOf(n: Int): List[Int] = digitsOf(n.toLong)
 
-  def digitsOf(n: Long): List[Int] = carryDigits(n)
-
-  def digitsOf(n: BigInt): List[Int] = carryBigDigits(n)
+  def digitsOf(n: Long): List[Int] = {
+    require(n >= 0, "Can only get digits of non-negative number")
+    carryDigits(n)
+  }
 
   @tailrec
   private def carryDigits(n: Long, carry: List[Int] = Nil): List[Int] = {
     if (n < 10) n :: carry
     else carryDigits(n / 10, (n % 10) :: carry)
+  }
+
+  def digitsOf(n: BigInt): List[Int] = {
+    require(n >= big0, "Can only get digits of non-negative number")
+    carryBigDigits(n)
   }
 
   @tailrec
@@ -54,6 +61,17 @@ object NumericFormat {
       val (d, r) = n /% big10
       carryBigDigits(d, r.toInt :: carry)
     }
+  }
+
+  def binaryDigitsOf(n: Long): List[Int] = {
+    require(n >= 0, "Can only get binary digits of non-negative number")
+    carryBinaryDigits(n)
+  }
+
+  @tailrec
+  private def carryBinaryDigits(n: Long, carry: List[Int] = Nil): List[Int] = {
+    if (n < 2) n :: carry
+    else carryBinaryDigits(n >> 1, (n & 1) :: carry)
   }
 
   def fromDigits(digits: Seq[Int]): Long = digits.foldLeft(0L) {_ * 10 + _}
